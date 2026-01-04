7b188ac26cc6fabc25cd8fa0fda8da7b58aceb54
FengmiAlice <2724876302@qq.com>
2026-01-04 10:58:42 +0800

    Fix:修改对话输入框样式问题
    Signed-off-by: FengmiAlice <2724876302@qq.com>

--- src/views/user/AiAnswer.js
+++ src/views/user/AiAnswer.js
@@ -1,175 +1,51 @@
 import  { useEffect, useState,useRef } from 'react';
-import { Button,  Avatar, Flex, Spin, Space,message,} from 'antd';
-import { Bubble,
-  Conversations,
-  Prompts,
-  Sender,
-  Welcome,
-  useXAgent,
-  useXChat,} from "@ant-design/x";
-import {   AppstoreAddOutlined,
-  CloudUploadOutlined,
-  CommentOutlined,
+import { Button, Switch, Avatar, Flex, Spin, Space,message,} from 'antd';
+import { Bubble,Conversations,Sender,Welcome,useXAgent,useXChat,XStream } from "@ant-design/x";
+import { 
   CopyOutlined,
   DeleteOutlined,
   DislikeOutlined,
   EditOutlined,
-  EllipsisOutlined,
-  FileSearchOutlined,
-  HeartOutlined,
   LikeOutlined,
-  PaperClipOutlined,
   PlusOutlined,
-  ProductOutlined,
   QuestionCircleOutlined,
   ReloadOutlined,
-  ScheduleOutlined,
   ShareAltOutlined,
-  SmileOutlined, } from '@ant-design/icons';
-
+  EllipsisOutlined
+  } from '@ant-design/icons';
 import dayjs from 'dayjs';
-//  const { useToken } = theme; // 获取主题色
-// const useStyle = () => { // 自定义样式
-//     const {token} =useToken();
-//     return {
-//         layout: css`
-//       width: 100%;
-//       min-width: 1000px;
-//       height: 100vh;
-//       display: flex;
-//       background: ${token.colorBgContainer};
-//       font-family: AlibabaPuHuiTi, ${token.fontFamily}, sans-serif;
-//     `,
-//         // sider 样式
-//         sider: css`
-//       background: ${token.colorBgLayout}80;
-//       width: 280px;
-//       height: 100%;
-//       display: flex;
-//       flex-direction: column;
-//       padding: 0 12px;
-//       box-sizing: border-box;
-//     `,
-//         logo: css`
-//       display: flex;
-//       align-items: center;
-//       justify-content: start;
-//       padding: 0 24px;
-//       box-sizing: border-box;
-//       gap: 8px;
-//       margin: 24px 0;
-
-//       span {
-//         font-weight: bold;
-//         color: ${token.colorText};
-//         font-size: 16px;
-//       }
-//     `,
-//         addBtn: css`
-//       background: #1677ff0f;
-//       border: 1px solid #1677ff34;
-//       height: 40px;
-//     `,
-//         conversations: css`
-//       flex: 1;
-//       overflow-y: auto;
-//       margin-top: 12px;
-//       padding: 0;
-
-//       .ant-conversations-list {
-//         padding-inline-start: 0;
-//       }
-//     `,
-//         siderFooter: css`
-//       border-top: 1px solid ${token.colorBorderSecondary};
-//       height: 40px;
-//       display: flex;
-//       align-items: center;
-//       justify-content: space-between;
-//     `,
-//         // chat list 样式
-//         chat: css`
-//       height: 100%;
-//       width: 100%;
-//       box-sizing: border-box;
-//       display: flex;
-//       flex-direction: column;
-//       padding-block: ${token.paddingLG}px;
-//       gap: 16px;
-//     `,
-//         chatPrompt: css`
-//       .ant-prompts-label {
-//         color: #000000e0 !important;
-//       }
-//       .ant-prompts-desc {
-//         color: #000000a6 !important;
-//         width: 100%;
-//       }
-//       .ant-prompts-icon {
-//         color: #000000a6 !important;
-//       }
-//     `,
-//         chatList: css`
-//       flex: 1;
-//       overflow: auto;
-//     `,
-//         loadingMessage: css`
-//       background-image: linear-gradient(90deg, #ff6b23 0%, #af3cb8 31%, #53b6ff 89%);
-//       background-size: 100% 2px;
-//       background-repeat: no-repeat;
-//       background-position: bottom;
-//     `,
-//         placeholder: css`
-//       padding-top: 32px;
-//     `,
-//         // sender 样式
-//         sender: css`
-//       width: 100%;
-//       max-width: 700px;
-//       margin: 0 auto;
-//     `,
-//         speechButton: css`
-//       font-size: 18px;
-//       color: ${token.colorText} !important;
-//     `,
-//         senderPrompt: css`
-//       width: 100%;
-//       max-width: 700px;
-//       margin: 0 auto;
-//       color: ${token.colorText};
-//     `,
-//     }
-// }
-const DEFAULT_CONVERSATIONS_ITEMS = [
-  {
-    key: 'default-0',
-    label: 'What is Ant Design X?',
-    group: 'Today',
-  },
-  {
-    key: 'default-1',
-    label: 'How to quickly install and import components?',
-    group: 'Today',
-  },
-  {
-    key: 'default-2',
-    label: 'New AGI Hybrid Interface',
-    group: 'Yesterday',
-  },
-];
+// const DEFAULT_CONVERSATIONS_ITEMS = [
+//   {
+//     key: 'default-0',
+//     label: 'What is Ant Design X?',
+//     group: 'Today',
+//   },
+//   {
+//     key: 'default-1',
+//     label: 'How to quickly install and import components?',
+//     group: 'Today',
+//   },
+//   {
+//     key: 'default-2',
+//     label: 'New AGI Hybrid Interface',
+//     group: 'Yesterday',
+//   },
+// ];
 const AiAnswer = () => {
-    // const  styles  = useStyle();
     const [inputValue, setInputValue] = useState("");
-    const abortController = useRef(null)
-    const [messageHistory, setMessageHistory] = useState({})
-    const [conversations, setConversations] = useState(DEFAULT_CONVERSATIONS_ITEMS)
-    const [curConversation, setCurConversation] = useState(DEFAULT_CONVERSATIONS_ITEMS[0].key)
-    const [attachmentsOpen, setAttachmentsOpen] = useState(false);
+    const abortController = useRef(null);
+    const [messageHistory, setMessageHistory] = useState({});
+    const [conversations, setConversations] = useState();
+    const [curConversation, setCurConversation] = useState();
+    const [activeConversation, setActiveConversation] = useState();
+    const [deepThink, setDeepThink] = useState(true);
+    const listRef = useRef(null);
+    const senderRef=useRef(null);
     // ==================== Runtime ====================
     const [agent] = useXAgent({
         baseURL: 'https://api.x.ant.design/api/llm_siliconflow_deepSeek-r1-distill-1wen-7b',
-        model: 'DeepSeek-R1-Distill-Qwen-7B',
-        dangerouslyApiKey: 'Bearer sk-xxxxxxxxxxxxxxxxxxxx',
+        model: 'qwen3',
+        stream: false
     });
     const loading = agent.isRequesting();
     const { onRequest, messages, setMessages } = useXChat({
@@ -232,8 +108,8 @@ const AiAnswer = () => {
         }
 
         onRequest({
-        stream: true,
-        message: { role: 'user', content: val },
+            stream: true,
+            message: { role: 'user', content: val },
         });
     };
      // ==================== Nodes ====================
@@ -284,13 +160,9 @@ const AiAnswer = () => {
                 className='conversations'
                 activeKey={curConversation}
                 onActiveChange={async (val) => {
-                abortController.current?.abort();
-                // The abort execution will trigger an asynchronous requestFallback, which may lead to timing issues.
-                // In future versions, the sessionId capability will be added to resolve this problem.
-                setTimeout(() => {
+                    abortController.current?.abort();
                     setCurConversation(val);
                     setMessages(messageHistory?.[val] || []);
-                }, 100);
                 }}
                 groupable
                 styles={{ item: { padding: '0 8px' } }}
@@ -310,14 +182,10 @@ const AiAnswer = () => {
                         const newList = conversations.filter((item) => item.key !== conversation.key);
                         const newKey = newList?.[0]?.key;
                         setConversations(newList);
-                        // The delete operation modifies curConversation and triggers onActiveChange, so it needs to be executed with a delay to ensure it overrides correctly at the end.
-                        // This feature will be fixed in a future version.
-                        setTimeout(() => {
                         if (conversation.key === curConversation) {
                             setCurConversation(newKey);
                             setMessages(messageHistory?.[newKey] || []);
                         }
-                        }, 200);
                     },
                     },
                 ],
@@ -332,32 +200,33 @@ const AiAnswer = () => {
 
     const chatList = (
         <div className='chatList'>
-        {messages?.length ? (
+        {messages?.length? (
             /* 🌟 消息列表 */
-            <Bubble.List
-            items={messages?.map((i) => ({
-                ...i.message,
-                classNames: {
-                    content: i.status === 'loading' ?  'loadingMessage'  : '',
-                },
-                typing: i.status === 'loading' ? { step: 5, interval: 20, suffix: <>💗</> } : false,
-            }))}
-            style={{ height: '100%', paddingInline: 'calc(calc(100% - 700px) /2)' }}
-            roles={{
-                assistant: {
-                placement: 'start',
-                footer: (
-                    <div style={{ display: 'flex' }}>
-                    <Button type="text" size="small" icon={<ReloadOutlined />} />
-                    <Button type="text" size="small" icon={<CopyOutlined />} />
-                    <Button type="text" size="small" icon={<LikeOutlined />} />
-                    <Button type="text" size="small" icon={<DislikeOutlined />} />
-                    </div>
-                ),
-                loadingRender: () => <Spin size="small" />,
-                },
-                user: { placement: 'end' },
-            }}
+                <Bubble.List
+                ref={listRef}
+                items={messages?.map((i) => ({
+                    ...i.message, // 消息内容
+                        classNames: {
+                            content: i.status === 'loading' ?  'loadingMessage'  : '',
+                        },
+                        typing: i.status === 'loading' ? { step: 5, interval: 20, suffix: <>💗</> } : false,
+                }))}
+                style={{ height: '100%', paddingInline: 'calc(calc(100% - 700px) /2)' }}
+                roles={{
+                    assistant: {
+                    placement: 'start',
+                    footer: (
+                        <div style={{ display: 'flex' }}>
+                        <Button type="text" size="small" icon={<ReloadOutlined />} />
+                        <Button type="text" size="small" icon={<CopyOutlined />} />
+                        <Button type="text" size="small" icon={<LikeOutlined />} />
+                        <Button type="text" size="small" icon={<DislikeOutlined />} />
+                        </div>
+                    ),
+                    loadingRender: () => <Spin size="small" />,
+                    },
+                    user: { placement: 'end' },
+                }}
             />
         ) : (
             <Space
@@ -384,10 +253,13 @@ const AiAnswer = () => {
     );
 
     const chatSender = (
+          /* 🌟 输入框 */
         <>
-            {/* 🌟 输入框 */}
             <Sender
                 value={inputValue}
+                key={curConversation}
+                ref={senderRef}
+                className='sender'
                 onSubmit={() => {
                     onSubmit(inputValue);
                     setInputValue('');
@@ -396,26 +268,28 @@ const AiAnswer = () => {
                 onCancel={() => {
                     abortController.current?.abort();
                 }}
-                prefix={
-                    <Button
-                        type="text"
-                        icon={<PaperClipOutlined style={{ fontSize: 18 }} />}
-                        onClick={() => setAttachmentsOpen(!attachmentsOpen)}
-                    />
-                }
-                loading={loading}
-                className='sender'
-                allowSpeech
+                autoSize={{ minRows: 3, maxRows: 6 }}
                 actions={(_, info) => {
-                    const { SendButton, LoadingButton, SpeechButton } = info.components;
+                const { SendButton, LoadingButton } = info.components;
                     return (
+                    <Flex justify="space-between" align="center">
+                        <Flex gap="small" align="center">
+                            <Switch
+                                checked={deepThink}
+                                onClick={(checked) => {
+                                    setDeepThink(checked);
+                                }}
+                                                checkedChildren="深度思考"
+                                                unCheckedChildren="普通模式"
+                            />
+                        </Flex>
                         <Flex gap={4}>
-                            <SpeechButton className='speechButton' />
                             {loading ? <LoadingButton type="default" /> : <SendButton type="primary" />}
                         </Flex>
+                    </Flex>
                     );
                 }}
-                placeholder="给我发信息吧"
+                placeholder="Press Enter to send message"
             />
         </>
     );
@@ -428,7 +302,14 @@ const AiAnswer = () => {
                 [curConversation]: messages,
             }));
         }
-    },[messages]);
+    }, [messages, curConversation]);
+    // 另一个 useEffect 用于聚焦
+    useEffect(() => {
+        // 当切换会话时，聚焦输入框
+        if (senderRef.current) {
+            senderRef.current.focus({cursor: 'end'});
+        }
+    }, [curConversation]); // 只在 curConversation 变化时聚焦
     return (
         <div className='layout'>
             {chatSider}
